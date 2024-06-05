1017d20a855bc338878d0bc5d7fcfbb49fddce6b
arghjain29 <arghjain1234@gmail.com>
2024-06-06 01:00:04 +0530

    yayy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+let name = "Argh jain";
 function App() {
   return (
-    // JSX code
     <>
-      <h1>Hello World</h1>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          Learn React
-          </a>
-        </header>
-      </div>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      
+      <div className="blank">
+        <h1>Hello {name}</h1>
+        <p1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi eligendi et expedita. Voluptates ullam quam sint consequuntur beatae neque unde sunt non qui quibusdam! Porro eum soluta voluptatibus possimus natus explicabo pariatur temporibus?
+        </p1>
+        </div>
     </>
   );
 }
