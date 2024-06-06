b22de875c7c46e6f56e0d3d2f1edd4cbc64984f2
arghjain29 <arghjain1234@gmail.com>
2024-06-06 17:52:14 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
-
-let name = "Argh jain";
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </nav>
-      
-      <div className="blank">
-        <h1>Hello {name}</h1>
-        <p1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi eligendi et expedita. Voluptates ullam quam sint consequuntur beatae neque unde sunt non qui quibusdam! Porro eum soluta voluptatibus possimus natus explicabo pariatur temporibus?
-        </p1>
-        </div>
+      <Navbar title="TextUtils" AboutText="About TextUtils" />
     </>
   );
 }
