70bd552e3a8626ae6c41a34d5a39f620ae738308
arghjain29 <arghjain1234@gmail.com>
2024-06-07 17:37:00 +0530

    aboutUs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" AboutText="About TextUtils" />
       
-      <div className="container mt-4">
+      {/* <div className="container mt-4">
       <TextForm header="Enter the Text to Analyze" />
-      </div>
+      </div> */}
+
+      <About />
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,21 +2,42 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
+
     const [text, setText] = useState('');
+
     const handleUpClick = () => {
-        console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText);
     }
     const handleLoClick = () => {
-        console.log("Lowercase was clicked");
         let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClear = () => {
+        let newText = '';
         setText(newText);
     }
     const handleOnChange = (event) => {
         console.log("handleOnChange was clicked");
         setText(event.target.value);
     }
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance(text);
+        const toggle = document.getElementById('toggle');
+        
+        msg.onend = () => {
+            toggle.innerHTML = "Speak";
+        };
+    
+        if (toggle.textContent === "Speak") {
+            window.speechSynthesis.speak(msg);
+            toggle.innerHTML = "Stop";
+        } else {
+            window.speechSynthesis.cancel();
+            toggle.innerHTML = "Speak";
+        }
+    };
+    
     const countWords = (text) => {
         return text.split(/\s+/).filter(word => word.length > 0).length;
     }
@@ -26,8 +47,12 @@ export default function TextForm(props) {
             <div className="mb-3 container">
                 <h1>{props.header}</h1>
                 <textarea className="form-control mt-3" value={text} onChange={handleOnChange} placeholder='Enter Text here' id="myBox" rows="8"></textarea>
-                <button className="btn btn-primary mt-4" onClick={handleUpClick} >Convert to upper Case</button>
-                <button className="btn btn-success mt-4 mx-2" onClick={handleLoClick} >Convert to lower Case</button>
+                <div className='d-flex'>
+                    <button className="btn btn-primary mt-4" onClick={handleUpClick} >Convert to upper Case</button>
+                    <button className="btn btn-success mt-4 mx-2" onClick={handleLoClick} >Convert to lower Case</button>
+                    <button onClick={speak} className="btn btn-warning mt-4 mx-2 ms-auto" id="toggle">Speak</button>
+                    <button className="btn btn-warning mt-4 mx-2" onClick={handleClear} >Clear Text</button>
+                </div>
             </div>
             <div className="container mt-3">
                 <h1>Your text Summary</h1>
