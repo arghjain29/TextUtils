9e4ab9984de0bfedc1d660b3220587e3d3399fc7
arghjain29 <arghjain1234@gmail.com>
2024-06-08 00:23:03 +0530

    ThemeMode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
-import About from './components/About';
+import React, { useState } from 'react';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
+
+  const[Mode,setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(Mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" AboutText="About TextUtils" />
+      <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} />
       
-      {/* <div className="container mt-4">
-      <TextForm header="Enter the Text to Analyze" />
-      </div> */}
+      <div className="container mt-4">
+      <TextForm header="Enter the Text to Analyze" mode={Mode}/>
+      </div>
 
-      <About />
+      {/* <About /> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,7 +36,12 @@ export default function TextForm(props) {
             window.speechSynthesis.cancel();
             toggle.innerHTML = "Speak";
         }
-    };
+    }
+    const handleCopy = () => {
+        let text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
     
     const countWords = (text) => {
         return text.split(/\s+/).filter(word => word.length > 0).length;
@@ -45,22 +50,25 @@ export default function TextForm(props) {
     return (
         <>
             <div className="mb-3 container">
-                <h1>{props.header}</h1>
-                <textarea className="form-control mt-3" value={text} onChange={handleOnChange} placeholder='Enter Text here' id="myBox" rows="8"></textarea>
+                <h1 style={{color: props.mode==='light'? 'black': 'white'}}>{props.header}</h1>
+                
+                <textarea className="form-control mt-3" value={text} style={{backgroundColor: props.mode==='light'? 'white': 'grey', color:props.mode==='light'? 'black': 'white'}} onChange={handleOnChange} placeholder='Enter Text here' id="myBox" rows="8"> </textarea>
+                
                 <div className='d-flex'>
-                    <button className="btn btn-primary mt-4" onClick={handleUpClick} >Convert to upper Case</button>
-                    <button className="btn btn-success mt-4 mx-2" onClick={handleLoClick} >Convert to lower Case</button>
+                    <button className="btn btn-primary mt-4" onClick={handleUpClick} >Convert to Upper Case</button>
+                    <button className="btn btn-primary mt-4 mx-2" onClick={handleLoClick} >Convert to Lower Case</button>
+                    <button className="btn btn-success mt-4 mx-2" onClick={handleCopy} >Copy Text</button>
                     <button onClick={speak} className="btn btn-warning mt-4 mx-2 ms-auto" id="toggle">Speak</button>
                     <button className="btn btn-warning mt-4 mx-2" onClick={handleClear} >Clear Text</button>
                 </div>
             </div>
-            <div className="container mt-3">
+            <div className="container mt-3" style={{color: props.mode==='light'? 'black': 'white'}}>
                 <h1>Your text Summary</h1>
                 <p>{countWords(text)} words and {text.length} characters</p>
                 <p>{0.008 * countWords(text)} Minutes read</p>
                 <br />
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter something to Preview here" }</p>
             </div>
         </>
     )
